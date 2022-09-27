c2a9cfd80bf4ba6199eaa7f3466f900c146a24a3
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-09-27 11:02:06 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO_Palashicheva_PR3_primer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            X.Text = "5,5";
+            Y.Text = "4,3";
+            Z.Text = "2,8";
+            Itog.Text = "Ст. гр. ИСП Палашичева 402";
+        }
+
+        private void Schet_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
